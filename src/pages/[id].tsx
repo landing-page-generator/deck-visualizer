1139b92077d514b997bfc39f82e1bc84803a0bcc
Viktor <viktor.gabaj@gmail.com>
2024-09-28 22:39:06 +0200

    feat: page layout

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,14 +1,37 @@
 import { Hero } from '@/components/Hero/Hero'
 
+import styles from "./styles.module.css"
 const Page = () => {
     return (
-        <>
-            <Hero
-                title="This is some title"
-                imageURL="https://images.pexels.com/photos/17887855/pexels-photo-17887855.jpeg"
-                subtitle="This is a subtitle"
-            />
-        </>
+        <div className={styles.root}>
+            <div className={styles.container}>
+                <Hero
+                    title="This is some title"
+                    imageURL="https://images.pexels.com/photos/17887855/pexels-photo-17887855.jpeg"
+                    subtitle="This is a subtitle"
+                />
+                <Hero
+                    title="This is some title"
+                    imageURL="https://images.pexels.com/photos/17887855/pexels-photo-17887855.jpeg"
+                    subtitle="This is a subtitle"
+                />
+                <Hero
+                    title="This is some title"
+                    imageURL="https://images.pexels.com/photos/17887855/pexels-photo-17887855.jpeg"
+                    subtitle="This is a subtitle"
+                />
+                <Hero
+                    title="This is some title"
+                    imageURL="https://images.pexels.com/photos/17887855/pexels-photo-17887855.jpeg"
+                    subtitle="This is a subtitle"
+                />
+                <Hero
+                    title="This is some title"
+                    imageURL="https://images.pexels.com/photos/17887855/pexels-photo-17887855.jpeg"
+                    subtitle="This is a subtitle"
+                />
+            </div>
+        </div>
     )
 }
 
