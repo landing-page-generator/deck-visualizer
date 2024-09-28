022bfa4257468698e7b2e3da231d2d6f008d3c0f
Domagoj Vukovic <off.vukovic@gmail.com>
2024-09-28 22:56:41 +0200

    faet: fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import type { AppProps } from "next/app"
+import { Raleway } from "next/font/google"
 import "./styles.css"
 
+const raleway = Raleway({
+    display: "block",
+    preload: false,
+    weight: ["500", "400"],
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <style jsx global>{`
+                html {
+                    font-family: ${raleway.style.fontFamily};
+                }
+            `}</style>
+            <Component {...pageProps} />
+        </>
+    )
 }
